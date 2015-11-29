f6ea1bb62117b91db104be07b800c457c77740f4
martinandert <mandert@gmail.com>
2015-11-29 14:19:54 +0100

    Update plugin to Babel 6

---
+++ test/index.js
@@ -0,0 +1,28 @@
+var path    = require('path');
+var fs      = require('fs');
+var assert  = require('assert');
+var babel   = require('babel-core');
+var plugin  = require('../');
+
+function trim(str) {
+  return str.replace(/^\s+|\s+$/, '');
+}
+
+describe('babel-plugin-dev', function() {
+  var fixturesDir = path.join(__dirname, 'fixtures');
+  var options = { plugins: [plugin] };
+
+  fs.readdirSync(fixturesDir).map(function(caseName) {
+    it('works for the ' + caseName + ' case', function() {
+      var fixtureDir = path.join(fixturesDir, caseName);
+
+      var actualPath    = path.join(fixtureDir, 'actual.js');
+      var expectedPath  = path.join(fixtureDir, 'expected.js');
+
+      var actual    = babel.transformFileSync(actualPath, options).code;
+      var expected  = fs.readFileSync(expectedPath, 'utf8');
+
+      assert.equal(trim(actual), trim(expected));
+    });
+  });
+});
