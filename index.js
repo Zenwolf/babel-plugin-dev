7c6f4a50b673fd37ba2b96c035a32d78d2b6d00c
Matthew Jaquish <mattjaq@gmail.com>
2016-01-05 16:20:29 -0800

    Update format and add eslint

--- index.js
+++ index.js
@@ -12,29 +12,29 @@
  * minifiers and to make it easier to write debug and dev-specific code.
  */
 module.exports = function(babel) {
-  var t = babel.types;
+    var t = babel.types;
 
-  var DEV_EXPRESSION = t.binaryExpression(
-    '!==',
-    t.stringLiteral('production'),
-    t.memberExpression(
-      t.memberExpression(
-        t.identifier('process'),
-        t.identifier('env'),
-        false
-      ),
-      t.identifier('NODE_ENV'),
-      false
-    )
-  );
+    var DEV_EXPRESSION = t.binaryExpression(
+        '!==',
+        t.stringLiteral('production'),
+        t.memberExpression(
+            t.memberExpression(
+                t.identifier('process'),
+                t.identifier('env'),
+                false
+            ),
+            t.identifier('NODE_ENV'),
+            false
+        )
+    );
 
-  return {
-    visitor: {
-      Identifier: function(path) {
-        if (t.isIdentifier(path.node, { name: '__DEV__'})) {
-          path.replaceWith(DEV_EXPRESSION);
+    return {
+        visitor: {
+            Identifier: function(path) {
+                if (t.isIdentifier(path.node, { name: '__DEV__'})) {
+                    path.replaceWith(DEV_EXPRESSION);
+                }
+            }
         }
-      }
-    }
-  };
+    };
 };
--- test/index.js
+++ test/index.js
@@ -5,24 +5,24 @@ var babel   = require('babel-core');
 var plugin  = require('../');
 
 function trim(str) {
-  return str.replace(/^\s+|\s+$/, '');
+    return str.replace(/^\s+|\s+$/, '');
 }
 
 describe('babel-plugin-dev', function() {
-  var fixturesDir = path.join(__dirname, 'fixtures');
-  var options = { plugins: [plugin] };
+    var fixturesDir = path.join(__dirname, 'fixtures');
+    var options = { plugins: [plugin] };
 
-  fs.readdirSync(fixturesDir).map(function(caseName) {
-    it('works for the ' + caseName + ' case', function() {
-      var fixtureDir = path.join(fixturesDir, caseName);
+    fs.readdirSync(fixturesDir).map(function(caseName) {
+        it('works for the ' + caseName + ' case', function() {
+            var fixtureDir = path.join(fixturesDir, caseName);
 
-      var actualPath    = path.join(fixtureDir, 'actual.js');
-      var expectedPath  = path.join(fixtureDir, 'expected.js');
+            var actualPath    = path.join(fixtureDir, 'actual.js');
+            var expectedPath  = path.join(fixtureDir, 'expected.js');
 
-      var actual    = babel.transformFileSync(actualPath, options).code;
-      var expected  = fs.readFileSync(expectedPath, 'utf8');
+            var actual    = babel.transformFileSync(actualPath, options).code;
+            var expected  = fs.readFileSync(expectedPath, 'utf8');
 
-      assert.equal(trim(actual), trim(expected));
+            assert.equal(trim(actual), trim(expected));
+        });
     });
-  });
 });
